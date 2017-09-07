dc8758da0f122022e06150b6db7b7f3d2dcd914b
Chris Zuber <shgysk8zer0@gmail.com>
2017-09-06 21:55:15 -0700

    Set listeners by named functions
    This prevents duplicate event handlers from being fired.
    Fixes #1

--- background.js
+++ background.js
@@ -1,17 +1,36 @@
+const TABS = {};
 function scanPage(tab) {
-	browser.runtime.onMessage.addListener(links => {
-		if (links.length !== 0) {
-			browser.pageAction.show(tab.tabId);
-			browser.pageAction.onClicked.addListener(() => {
-				browser.tabs.create({
-					url: links[0].href
-				});
-			});
-		}
-	});
-	browser.tabs.sendMessage(tab.tabId, {});
+	browser.runtime.onMessage.addListener(messageHandler);
+	browser.tabs.sendMessage(tab.id, {id: tab.id, title: tab.title, url: tab.url});
 }
 
-browser.tabs.onCreated.addListener(scanPage);
-browser.tabs.onActivated.addListener(scanPage);
-browser.tabs.onUpdated.addListener(scanPage);
+function messageHandler(tab) {
+	TABS[tab.id] = tab;
+	if (tab.links.length !== 0) {
+		browser.pageAction.show(tab.id);
+		browser.pageAction.onClicked.addListener(clickHandler);
+	}
+}
+
+function clickHandler(tab) {
+	browser.tabs.create({
+		url: TABS[tab.id].links[0].href
+	});
+}
+
+function updateHandler(tabId) {
+	browser.tabs.get(tabId).then(tab => {
+		if (tab.status === 'complete') {
+			scanPage(tab);
+		}
+	});
+}
+
+function removeHandler(tabId) {
+	delete TABS[tabId];
+}
+
+// browser.tabs.onCreated.addListener(console.log);
+// browser.tabs.onActivated.addListener(scanPage);
+browser.tabs.onUpdated.addListener(updateHandler);
+browser.tabs.onDetached.addListener(removeHandler);

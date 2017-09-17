77fab0ab519fe68f4013c5538245509d088243b0
Chris Zuber <shgysk8zer0@gmail.com>
2017-09-17 12:16:40 -0700

    Append feed links to document.body Resolves #5

--- popup.js
+++ popup.js
@@ -1,20 +1,21 @@
 function init() {
 	const url = new URL(location.href);
 	const links = JSON.parse(url.searchParams.get('links'));
-	const container = document.createElement('div');
+	const container = document.body;
 
 	try {
-		links.forEach(link => {
+		links.forEach((link, index) => {
 			let feed = document.createElement('a');
 			feed.href = link.href;
 			feed.textContent = link.title;
 			feed.target = '_blank';
 			container.appendChild(feed);
+			if (links.length - 1 > index) {
+				container.appendChild(document.createElement('br'));
+			}
 		});
 	} catch (error) {
 		console.error(error);
-	} finally {
-		console.info(container.innerHTML);
 	}
 
 }

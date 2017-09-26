7adf1f0bda3cbfd174b184a14851f395e34959e6
Chris Zuber <shgysk8zer0@gmail.com>
2017-09-26 00:51:31 -0700

    Code cleanup
    - Use `const` to alias `browser.storage.local` (to simplify possible
    changes)
    - Filter out unchecked checkboxes by querying `:not(:checked)`
    - Get current opts using `await` instead of using `Promise`

--- options.js
+++ options.js
@@ -1,45 +1,50 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
 	function $(selector, base = document) {
-		return Array.from(base.querySelectorAll(selector));
+		return [...base.querySelectorAll(selector)];
 	}
-	browser.storage.local.get().then(opts => {
-		const inputs = $('[name]', document.forms.options);
-		Object.keys(opts).forEach(key => {
-			const input = inputs.find(el => el.name === key);
-			if (input instanceof HTMLInputElement) {
-				switch(input.type) {
-				case 'checkbox':
-					input.checked = true;
-					break;
-				default:
-					input.value = opts[key];
-				}
-			} else if (input instanceof HTMLSelectElement) {
+
+	const storage = browser.storage.local;
+	const opts = await storage.get();
+	const inputs = $('[name]', document.forms.options);
+
+	Object.keys(opts).forEach(key => {
+		const input = inputs.find(el => el.name === key);
+		if (input instanceof HTMLInputElement) {
+			switch(input.type) {
+			case 'checkbox':
+				input.checked = true;
+				break;
+			default:
 				input.value = opts[key];
-			} else {
-				browser.storage.local.remove(key);
 			}
-		});
+		} else if (input instanceof HTMLSelectElement) {
+			input.value = opts[key];
+		} else {
+			storage.remove(key);
+		}
 	});
 
 	document.forms.options.addEventListener('submit', async (submit) => {
 		submit.preventDefault();
 		const form = new FormData(submit.target);
-		const opts = await browser.storage.local.get();
 
-		[...form.keys()].forEach(key => {
-			opts[key] = form.get(key);
+		[...form.keys()].forEach(key => opts[key] = form.get(key));
+
+		$('input[type="checkbox"]:not(:checked)', submit.target).forEach(input => {
+			delete opts[input.name];
+			storage.remove(input.name);
 		});
-		$('input[type="checkbox"]', submit.target).forEach(input => {
-			if (! input.checked) {
-				delete opts[input.name];
-				browser.storage.local.remove(input.name);
+		storage.set(opts);
+	});
+
+	$('[type="reset"]').forEach(btn => {
+		btn.addEventListener('click', click => {
+			click.preventDefault();
+			if (confirm('This will clear your settings')) {
+				btn.closest('form').reset();
+				storage.clear();
 			}
 		});
-		browser.storage.local.set(opts);
-	});
-	$('[type="reset"]').forEach(btn => {
-		btn.addEventListener('click', () => browser.storage.local.clear());
 	});
 	document.forms.options.hidden = false;
 }, {once: true});

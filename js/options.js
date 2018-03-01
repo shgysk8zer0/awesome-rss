6f7be4ea897e9e4766a35028158ff33704ec5552
Chris Zuber <shgysk8zer0@gmail.com>
2018-03-01 12:07:59 -0800

    Update resetting options form
    - Move setting form values to own function
    - Set form values on page load
    - Send message to reset options on form reset
    - Update form values after reset

--- js/options.js
+++ js/options.js
@@ -24,47 +24,51 @@ window.addEventListener('DOMContentLoaded', async () => {
 		}
 	}
 
-	const storage = browser.storage.sync;
-	const opts = await storage.get();
-	const form = document.forms.options;
-	const inputs = $('[name]', form);
+	async function setValuesFromStorage({
+		storage = browser.storage.sync,
+		form = document.forms.options,
+		opts
+	}) {
+		const inputs = $('[name]', form);
 
-	$('[data-locale-text]', form).forEach(getText);
-	$('[data-locale-title]', form).forEach(getTitle);
-	$('[data-locale-placeholder]', form).forEach(getPlaceholder);
-
-	Object.entries(opts).forEach(([key, value]) => {
-		const matches = inputs.filter(el => el.name === key);
-		matches.forEach(input => {
-			if (input instanceof HTMLInputElement) {
-				switch(input.type) {
-				case 'checkbox':
-					input.checked = true;
-					break;
-				case 'radio':
-					if (input.value === value) {
+		Object.entries(opts).forEach(([key, value]) => {
+			const matches = inputs.filter(el => el.name === key);
+			matches.forEach(input => {
+				if (input instanceof HTMLInputElement) {
+					switch(input.type) {
+					case 'checkbox':
 						input.checked = true;
+						break;
+					case 'radio':
+						if (input.value === value) {
+							input.checked = true;
+						}
+						break;
+					default:
+						input.value = opts[key];
 					}
-					break;
-				default:
+				} else if (input instanceof HTMLSelectElement) {
 					input.value = opts[key];
+				} else {
+					storage.remove(key);
 				}
-			} else if (input instanceof HTMLSelectElement) {
-				input.value = opts[key];
-			} else {
-				storage.remove(key);
-			}
+			});
 		});
+	}
 
+	const storage = browser.storage.sync;
+	const form = document.forms.options;
+	const opts = await storage.get();
 
-		$('output[for]', form).forEach(output => {
-			const input = document.getElementById(output.getAttribute('for'));
-			input.addEventListener('input', change => output.textContent = change.target.value);
-			output.textContent = input.value;
-		});
+	setValuesFromStorage({form, storage, opts});
+
+	$('output[for]', form).forEach(output => {
+		const input = document.getElementById(output.getAttribute('for'));
+		input.addEventListener('input', change => output.textContent = change.target.value);
+		output.textContent = input.value;
 	});
 
-	inputs.forEach(input => {
+	$('[name]', form).forEach(input => {
 		input.addEventListener('change', change => {
 			if (change.target instanceof HTMLInputElement) {
 				switch (change.target.type) {
@@ -82,12 +86,23 @@ window.addEventListener('DOMContentLoaded', async () => {
 		});
 	});
 
+	$('[data-locale-text]', form).forEach(getText);
+	$('[data-locale-title]', form).forEach(getTitle);
+	$('[data-locale-placeholder]', form).forEach(getPlaceholder);
+
 	form.addEventListener('submit', submit => submit.preventDefault());
 	form.addEventListener('reset', reset => {
+		reset.preventDefault();
 		if (confirm('This will clear your settings')) {
-			storage.clear();
-		} else {
-			reset.preventDefault();
+			browser.runtime.sendMessage({type: 'resetOpts'});
+
+			setTimeout(async () => {
+				setValuesFromStorage({
+					form,
+					storage,
+					opts: await storage.get(),
+				});
+			}, 500);
 		}
 	});
 	form.hidden = false;
